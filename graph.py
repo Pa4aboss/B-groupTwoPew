d7b24b3f4e49d2de275c024ffe655586aedbc4d4
Pa4aboss <Pa4aboss@users.noreply.github.com>
2017-03-14 13:32:58 +1000

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,38 +1,52 @@
 import graphviz
 
 
-class Node:
+class Node:    
     def __init__(self, num, obj=None):
+        #Параметры узла
         self.data = obj
+        #Номер узла
         self.num = num
 
 
 class Graph:
     def __init__(self, directed=False):
+        #список смежности
         self.graph = dict()
+	    #список вершин
         self.vertices = dict()
+	    #ориентированный граф или нет
         self.undirected = not directed
+	    #Начало пути в графе
+        self.start = 0
+	    # конец пути
+        self.finish = 0
 
     def add_node(self, e1):
+	#Добовляем узел
         self.vertices[e1.num] = e1
 
     def add_edge(self, e1, e2):
+        #Добовляем вершины
         if e1 not in self.vertices:
             self.vertices[e1.num] = e1
 
         if e2 not in self.vertices:
             self.vertices[e2.num] = e2
-
+        
+        #Заполняем список смежности
         if e1.num not in self.graph:
             self.graph[e1.num] = []
 
         self.graph[e1.num].append(e2.num)
+        #Если граф не ориентированый, добавляем обратное ребро
         if self.undirected:
             if e2.num not in self.graph:
                 self.graph[e2.num] = []
             self.graph[e2.num].append(e1.num)
 
     def dfs(self, start, visited=None, sort=None):
+    #Поиск в глубину
         if visited is None:
             visited = []
         if sort is None:
@@ -51,7 +65,7 @@ class Graph:
         return visited
 
     def dfs1(self, cl, p, v, st_end):
-        # Поиск в глубину для поиска цикла
+    # Поиск в глубину для поиска цикла
         cl[v] = 1
         if v not in self.graph:
             cl[v] = 2
@@ -128,17 +142,19 @@ class Graph:
         return num_comps
 
     def draw_graph(self, graph_name, extension):
-        drew = []
+     #Отрисовка графа
+        drew = [] #массив уже отрисованных ребер
+        #Проверка ориентированости
         if self.undirected:
             dot = graphviz.Graph(comment=graph_name, format=extension, engine='dot')
         else:
             dot = graphviz.Digraph(comment=graph_name, format=extension, engine='dot')
         for i in self.vertices:
-            dot.node(str(i))
+            dot.node(str(i)) #Рисуем вершины
         for u in self.graph:
             for v in self.graph[u]:
                 if self.undirected and [v, u] in drew:
                     continue
-                drew.append([u, v])
+                drew.append([u, v]) #Отрисовка ребер
                 dot.edge(str(u), str(v))
         dot.render(graph_name, view=True)
